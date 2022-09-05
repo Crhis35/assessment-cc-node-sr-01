d9e4f767adb9133aa211a519e195079056c182cc
Thiago Galvani <tgalvani@fullstacklabs.co>
2022-09-05 15:22:00 -0300

    feat: creating the monster routes

---
+++ src/router/monster.routes.ts
@@ -0,0 +1,17 @@
+import { Request, Response, Router } from 'express';
+import multer from 'multer';
+import { MonsterController } from '../controllers/monster.controller';
+
+const router = Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+router.get('/', (req: Request, res: Response) => res.send('monsters'));
+router.post('/', MonsterController.create);
+router.post('/import', upload.single('monsters'), MonsterController.importCsv);
+router.get('/:id', MonsterController.get);
+router.put('/:id', MonsterController.update);
+router.delete('/:id', MonsterController.remove);
+
+export default router;
