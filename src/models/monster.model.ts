f85f09d1fdacfb03cc95d1703425bbb9ff731587
Thiago Galvani <tgalvani@fullstacklabs.co>
2022-09-05 15:17:51 -0300

    feat: adding the monster model

---
+++ src/models/monster.model.ts
@@ -0,0 +1,20 @@
+import { Id, RelationMappings, RelationMappingsThunk } from 'objection';
+import Base from './base';
+import { Battle } from './battle.model';
+
+export class Monster extends Base {
+  id!: Id;
+  name!: string;
+  attack!: number;
+  defense!: number;
+  hp!: number;
+  speed!: number;
+  imageUrl!: string;
+  battles?: Battle[];
+
+  static tableName = 'monster';
+
+  static get relationMappings(): RelationMappings {
+    return {};
+  }
+}
