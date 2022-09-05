a08ca09594fb13fd8a03a88ed9c8b79f8b5d159b
Thiago Galvani <tgalvani@fullstacklabs.co>
2022-09-05 18:40:36 -0300

    refactor: removing the endpoint for make a battle

--- src/router/battle.routes.ts
+++ src/router/battle.routes.ts
@@ -1,9 +1,8 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
 import { BattleController } from '../controllers/battle.controller';
 
 const router = Router();
 
 router.get('/', BattleController.list);
-router.post('/', (req: Request, res: Response) => res.send('battle'));
 
 export default router;
