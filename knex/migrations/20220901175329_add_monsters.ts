c1f9af8d2742c9e4e10810baabfcd4ecda3b5916
Thiago Galvani <tgalvani@fullstacklabs.co>
2022-09-07 11:36:21 -0300

    fix: adjusting migration file

--- knex/migrations/20220901175329_add_monsters.ts
+++ knex/migrations/20220901175329_add_monsters.ts
@@ -5,7 +5,6 @@ export const up = (knex: Knex): Promise<void> =>
   knex.schema.createTable(Monster.tableName, (table: Knex.TableBuilder) => {
     table.increments();
     table.timestamps();
-    table.string('name').notNullable();
     table.string('imageUrl').notNullable();
     table.integer('attack').notNullable();
     table.integer('defense').notNullable();
