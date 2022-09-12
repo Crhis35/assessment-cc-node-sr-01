1493d388d678e87850e5582dde82d5e85d8a1081
Thiago Galvani <tgalvani@fullstacklabs.co>
2022-09-12 09:53:43 -0300

    fix: removing unused code

--- src/router/monster.routes.ts
+++ src/router/monster.routes.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
 import multer from 'multer';
 import { MonsterController } from '../controllers/monster.controller';
 
